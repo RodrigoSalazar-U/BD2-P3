b1224478f0cd469699e2f874917c3ba014bb5816
tux550 <rodrigosalad@gmail.com>
2022-11-15 15:34:03 -0500

    Avance

---
+++ highd.py
@@ -0,0 +1,12 @@
+import numpy as np
+from config import *
+from images import ImageLoader, get_image_distance, get_image_vector
+
+#LSH:
+#https://pypi.org/project/LocalitySensitiveHashing/?
+
+#KDTree:
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.KDTree.html
+
+#PCA:
+#https://towardsdatascience.com/pca-using-python-scikit-learn-e653f8989e60
